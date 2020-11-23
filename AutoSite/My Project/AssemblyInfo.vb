4ea8c0dcedba770233194fc735c8b1a939dedd35
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2020-11-22 23:00:05 -0600

    RC5 versioning

--- AutoSite/My Project/AssemblyInfo.vb
+++ AutoSite/My Project/AssemblyInfo.vb
@@ -8,7 +8,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("AutoSite")>
-<Assembly: AssemblyDescription("RC4")> 
+<Assembly: AssemblyDescription("RC5")> 
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("AutoSite")>
 <Assembly: AssemblyCopyright("Copyright © dotcomboom 2020")>

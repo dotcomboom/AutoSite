570dabc2e4470c33ad4f2b55051a1564f9b793e0
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2025-05-13 23:32:23 -0500

    Update version no. for recognition

--- Apricot/My Project/AssemblyInfo.vb
+++ Apricot/My Project/AssemblyInfo.vb
@@ -11,7 +11,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("Apricot")>
-<Assembly: AssemblyCopyright("Copyright © dotcomboom 2020")>
+<Assembly: AssemblyCopyright("Copyright © dotcomboom 2025")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.1.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
